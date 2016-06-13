79e610002ae8be1899e7501ce23b57684ad6d96d
MasterCassim <MasterCassim@users.noreply.github.com>
2016-06-13 16:23:48 +0200

    Updated consors_visa_reports.js with formatted code.

--- consors_visa_reports.js
+++ consors_visa_reports.js
@@ -8,44 +8,42 @@
 // @include      https://www.consorsbank.de/ev/-?docId=1339272*
 // @grant        none
 // ==/UserScript==
-
 // @require http://code.jquery.com/jquery-latest.js
-
 (function() {
     'use strict';
 
-        jQuery('form > .evr-date-range-days').after('<a href="javascript:void(0);" class="button primary">Hide VISA</a>');
-	    jQuery('form > .evr-date-range-days').after('<a href="javascript:void(0);" style="margin-left: 30px;" class="button primary">Hide Normal</a>');
+    jQuery('form > .evr-date-range-days').after('<a href="javascript:void(0);" class="button primary">Hide VISA</a>');
+    jQuery('form > .evr-date-range-days').after('<a href="javascript:void(0);" style="margin-left: 30px;" class="button primary">Hide Normal</a>');
 
-	        var normalButton = jQuery('form > .evr-date-range-days').next();
-		    var visaButton = normalButton.next();
+    var normalButton = jQuery('form > .evr-date-range-days').next();
+    var visaButton = normalButton.next();
 
-		        normalButton.click(function() {
-			        if(normalButton.text() == 'Show Normal') {
-				            normalButton.text('Hide Normal');
-					            } else {
-						                normalButton.text('Show Normal');
-								        }
+    normalButton.click(function() {
+        if (normalButton.text() == 'Show Normal') {
+            normalButton.text('Hide Normal');
+        } else {
+            normalButton.text('Show Normal');
+        }
 
-									        jQuery('tr.abaxx-list-item').each(function(index, element) {
-										            if(jQuery(element).find('.ev-table-cell-secondary-data').text().toLowerCase().indexOf('visa') === -1) {
-											                    jQuery(element).toggle();
-													                }
-															        });
-																    });
+        jQuery('tr.abaxx-list-item').each(function(index, element) {
+            if (jQuery(element).find('.ev-table-cell-secondary-data').text().toLowerCase().indexOf('visa') === -1) {
+                jQuery(element).toggle();
+            }
+        });
+    });
 
-																        visaButton.click(function() {
-																	        if(visaButton.text() == 'Show VISA') {
-																		            visaButton.text('Hide VISA');
-																			            } else {
-																				                visaButton.text('Show VISA');
-																						        }
+    visaButton.click(function() {
+        if (visaButton.text() == 'Show VISA') {
+            visaButton.text('Hide VISA');
+        } else {
+            visaButton.text('Show VISA');
+        }
 
-																							        jQuery('tr.abaxx-list-item').each(function(index, element) {
-																								            if(jQuery(element).find('.ev-table-cell-secondary-data').text().toLowerCase().indexOf('visa') > -1) {
-																									                    jQuery(element).toggle();
-																											                }
-																													        });
-																														    });
+        jQuery('tr.abaxx-list-item').each(function(index, element) {
+            if (jQuery(element).find('.ev-table-cell-secondary-data').text().toLowerCase().indexOf('visa') > -1) {
+                jQuery(element).toggle();
+            }
+        });
+    });
 
-																														    })();
+})();
